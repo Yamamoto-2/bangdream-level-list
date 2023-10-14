849e3f6ef3a4a4f5803a029ae02892c03d03c372
Yamamoto-2 <1810972564qq@gmail.com>
2023-10-14 14:01:44 +0900

    fix: change font size of header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -2,15 +2,15 @@
     <div id="header">
         <img class="header-image" src="/images/header.png" alt="header">
         <div class="header-text">
-            <div style="font-size: 24px; line-height: 1.5;">
+            <div style="font-size: 23px; line-height: 1.5;">
                 评级表说明：<br>
                 本表收录<u>旧表日服已实装曲目以及当前版本为止所复评曲目</u>，表中为歌曲实际难度排序。当前版本号：v2.38<br><br>
             </div>
-            <div style="font-size: 24px; line-height: 1.5">
+            <div style="font-size: 23px; line-height: 1.5">
                 组长：绀野纯子（鹤丸/水月）<br>
                 副组长：源樱（极·冰乱舞/挖矿人）、镜影Bly
             </div>
-            <div style="font-size: 24px; line-height: 1.5">
+            <div style="font-size: 23px; line-height: 1.5">
                 谱面组成员：栗薯/栗子、幽雨、脆饼、AI黑头子、糸見結希、海葵、碳铝、xcw、秋葉亜里沙、霜凡、张三、爱丽丝（防沉迷中）、soul、遥情幻想、楠木、无名K、XLF笑凌风、麻子/麻薯、白井黑子、核平、Shin、红二、辰空、音羽、音叉、TF_Snow、咲希、雪梨、瓜皮、炫蓝xl、风花雪月、企鹅、MISS喵子呀、水野爱（zcwise，已退休）、坏坏（已退休）、庭牧（已退休）、丹提诺艾（已退休）（排名不分先后）
             </div>
         </div>
